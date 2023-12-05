3a3dfae3975bf86ecfdf6b27035e801cc3e8ea69
zhuuu98 <liao.yingzhu98@gmail.com>
2023-12-05 16:30:19 +0800

    1205 1630

--- js/javascript.js
+++ js/javascript.js
@@ -12,3 +12,18 @@ function minuser(){
     }    
     document.getElementById("countnum").innerHTML=count;
 }
+
+function adder2(){
+    var count2=document.getElementById("countnum2").innerHTML;
+    count2=parseInt(count2)+1;
+    document.getElementById("countnum2").innerHTML=count2;
+}
+function minuser2(){
+    var count2=document.getElementById("countnum2").innerHTML;
+    if(count2<=0){
+        count2=0;
+    }else{
+        count2=parseInt(count2)-1;
+    }    
+    document.getElementById("countnum2").innerHTML=count2;
+}
