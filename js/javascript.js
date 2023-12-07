448fc7aa3bf30de8cdf3c0cbe4324497f26b1669
zhuuu98 <liao.yingzhu98@gmail.com>
2023-12-08 00:50:41 +0800

    1208 2450

--- js/javascript.js
+++ js/javascript.js
@@ -1,34 +1,3 @@
-// function adder(){
-//     var count=document.getElementById("countnum").innerHTML;
-//     count=parseInt(count)+1;
-//     document.getElementById("countnum").innerHTML=count;
-// }
-// function minuser(){
-//     var count=document.getElementById("countnum").innerHTML;
-//     if(count<=0){
-//         count=0;
-//     }else{
-//         count=parseInt(count)-1;
-//     }    
-//     document.getElementById("countnum").innerHTML=count;
-// }
-
-// function adder2(){
-//     var count2=document.getElementById("countnum2").innerHTML;
-//     count2=parseInt(count2)+1;
-//     document.getElementById("countnum2").innerHTML=count2;
-// }
-// function minuser2(){
-//     var count2=document.getElementById("countnum2").innerHTML;
-//     if(count2<=0){
-//         count2=0;
-//     }else{
-//         count2=parseInt(count2)-1;
-//     }    
-//     document.getElementById("countnum2").innerHTML=count2;
-// }
-
-
 function init(){
     let btnMinus = document.getElementsByClassName("minus");
     let btnPlus = document.getElementsByClassName("plus");
