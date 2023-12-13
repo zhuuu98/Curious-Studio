a62293f5ac54e815566af79509692d7fde29debf
zhuuu98 <liao.yingzhu98@gmail.com>
2023-12-14 01:01:20 +0800

    1214 0101

--- js/jQuery.js
+++ js/jQuery.js
@@ -52,7 +52,7 @@ $(function () {
     // 檢查視窗寬度，只在寬度大於 768px 時執行
     if ($(window).width() > 768) {
         $('.index-mb-header').css({
-            display: 'block',
+            // display: 'block',
             opacity: 0,
         });
 
@@ -76,99 +76,5 @@ $(function () {
 
 
 
-
-
-
-
-// $(function () {
-//     let showHeight = 200
-
-//     $('.aa').css({
-//         display: 'block',
-//         opacity: 0,
-//     })
-
-//     $(window).scroll(function(){
-//         $('.aa').each(function(){
-//             let areaTop = $(this).offset().top  //物件和螢幕的距離
-
-//             if($(window).scrollTop() >= (areaTop + showHeight) - $(window).height()){
-//                 $(this).stop().animate({
-//                     opacity: 1,
-//                 }, 300)
-//             }else{
-//                 $(this).stop().animate({
-//                     opacity: 0,
-//                 }, 500)
-//             }
-//         })
-//     })
-// })	
-
-
-
-
-
-// $(function () {
-//     let showHeight = 900
-//     let windowWidth = $(window).width();
-// }
-
-//     if($(window).width() > 768){
-//         $('.index-mb-header').css({
-//                 display: 'block',
-//                 opacity: 0,
-//             })
-
-//         $(window).scroll(function(){
-//             $('.index-mb-header').(function(){
-//                 let areaTop = $(this).offset().top  //物件和螢幕的距離
-//                 // console.log(areaTop);
-
-//                 if($(window).scrollTop() >= (areaTop + showHeight) - $(window).height()){
-//                     $(this).stop().animate({
-//                         opacity: 1,
-//                     }, 500)
-//                 }else{
-//                     $(this).stop().animate({
-//                         opacity:0,
-//                     }, 500)
-//                 }
-//             })
-//         })
-// })	
-
-
-
-
-
-// $(function () {
-//     let showHeight = 400;
-
-//     $(window).scroll(function(){
-//         let windowWidth = $(window).width(); // 移至 scroll 事件中以動態獲取視窗寬度
-
-//         if(windowWidth > 768){
-//             $('.index-mb-header').css({
-//                 display: 'block',
-//                 opacity: 0,
-//             });
-
-//             $('.index-mb-header').each(function(){
-//                 let indexMbHeaderTop = $(this).offset().top; // 物件和螢幕的距離
-
-//                 if($(window).scrollTop() >= (indexMbHeaderTop + showHeight) - $(window).height()){
-//                     $(this).stop().animate({
-//                         opacity: 1,
-//                     }, 500);
-//                 } else {
-//                     $(this).stop().animate({
-//                         opacity: 0,
-//                     }, 500);
-//                 }
-//             });
-//         }
-//     });
-// });
 
 
