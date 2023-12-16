1fbd1995fc36b2e3853aa6e04bc9cb45bf955555
zhuuu98 <liao.yingzhu98@gmail.com>
2023-12-16 19:56:59 +0800

    1216 1956

--- js/jQuery.js
+++ js/jQuery.js
@@ -76,5 +76,55 @@ $(function () {
 
 
 
+//首頁slider
+
+$(function () {
+    let divWidth = $('#sliderBoard').width() //div的寬度 = li的寬度
+    let imgCount = $('#content li').length //計算有幾張圖片
+
+    for(let i = 0; i < imgCount; i++){
+        $('#contentButton').append(`<li></li>`) //增加圓圈的li標籤
+    }
+    $('#contentButton li:first').addClass('clicked') //針對圓圈li的第一個小孩增加class : clicked 
+
+    $('#content li').width(divWidth)   //li的寬度
+    $('#content').width(divWidth * imgCount)   //ul的寬度 = li的個數 * li的寬度
+
+
+    let index = 0
+    let timer = setInterval(moveToNext, 3000)
+
+    $('#contentButton li').click(function(){
+        clearInterval(timer)
+        index = $(this).index()
+
+        $('#content').animate({
+            left: divWidth * index * -1,
+        })
+
+        $(this).addClass('clicked')
+        $('#contentButton li').not(this).removeClass('clicked')
+
+        timer = setInterval(moveToNext, 3000)
+    })
+
+    function moveToNext(){
+        if(index < imgCount - 1){
+            index++
+        }else{
+            index = 0
+        }
+
+        $('#content').animate({
+            left: divWidth * index * -1,
+        })
+
+        $(`#contentButton li:eq(${index})`).addClass('clicked')
+        $('#contentButton li').not(`:eq(${index})`).removeClass('clicked')
+    }
+});
+
+
+
 
 
